513c18fbb084b0682ada5b044ef57596a638115c
Chpokman <cusenov785@gmail.com>
2019-12-16 14:53:30 +0600

    Changed the usual links in the header to links

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -10,9 +10,10 @@ import {
   Container
 } from 'reactstrap';
 import '../styles/main.css';
+import {Link} from 'react-router-dom';
 import logo from '../assets/icon/image 54.png';
 
-const Example = (props) => {
+const Example = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   const toggle = () => setIsOpen(!isOpen);
@@ -26,16 +27,32 @@ const Example = (props) => {
 
             <Nav className="mr-auto" navbar>
               <NavItem className="first item">
-                <NavLink href="/aboutproject">О проекте</NavLink>
+                <NavLink tag={"p"}>
+                  <Link to={"/aboutproject"}>
+                    О проекте
+                  </Link>
+                </NavLink>
               </NavItem>
               <NavItem className = "item ">
-                <NavLink href="/lessons">Уроки</NavLink>
+                <NavLink tag={"p"}>
+                  <Link to="/lessons">
+                    Уроки
+                  </Link>
+                </NavLink>
               </NavItem>
               <NavItem className = "item">
-                <NavLink href="/news">Новости</NavLink>
+                <NavLink tag={"p"}>
+                  <Link to="/news">
+                    Новости
+                  </Link>
+                </NavLink>
               </NavItem>
               <NavItem className = "item">
-                <NavLink href="/contacts">Контакты</NavLink>
+                <NavLink tag={"p"}>
+                  <Link to="/contacts">
+                    Контакты
+                  </Link>
+                </NavLink>
               </NavItem>
            
             </Nav>
@@ -43,6 +60,6 @@ const Example = (props) => {
         </Container>
       </Navbar>
   );
-}
+};
 
 export default Example;
