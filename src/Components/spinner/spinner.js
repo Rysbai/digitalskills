97c7138d05470f8f112540d7add303cd9e86f647
Chpokman <cusenov785@gmail.com>
2020-01-07 22:43:31 +0600

    Adapted the main page.

---
+++ src/Components/spinner/spinner.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./spiner.css"
+
+const Spinner = () =>{
+  return(
+    <div className="load">
+      <hr /><hr /><hr /><hr />
+    </div>
+
+  )
+}
+export default Spinner;
